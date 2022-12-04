3161d7d8527b18f38c555ccd4ead8683ada0d380
henrybirch <hab.b@hotmail.co.uk>
2022-12-04 14:15:37 +0000

    import scalascraper funcs/objs

--- drugscraper/src/main/scala/GetDataFrame/DrugScrape.scala
+++ drugscraper/src/main/scala/GetDataFrame/DrugScrape.scala
@@ -1,3 +1,9 @@
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import net.ruippeixotog._
+
 object DrugScrape {
-     
+  val browser = JsoupBrowser()
 }
